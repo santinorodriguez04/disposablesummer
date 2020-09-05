cb2fd3ebbd3a08e0993cac55cafe2c4505d4608f
Admin <disposablesummer_admin@glendale.dreamhost.com>
2020-09-05 10:47:57 -0700

    Updated from Santino is awesome to Brian is awesome and added a cool pic of Santino

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <title>Santino is Awesome!</title>
 </head>
 <body>
-<?php echo '<p>Santino is awesome!!</p>'; ?>
+<?php echo '<p>Brian is awesome!!</p>'; ?>
+<img src="images/IMG_0202.JPG"/>
 </body>
 </html>
