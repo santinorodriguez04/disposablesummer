ad68a140f36d291080faebcd8dc6d78aceb1e475
Jessica Rodriguez <santinorodriguez04@gmail.com>
2020-09-05 14:51:20 -0400

    changed it to yeah!!

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Santino is Awesome!</title>
 </head>
 <body>
-<?php echo '<p>Sorry, made a mistake. Santino is really awesome!!</p>'; ?>
+<?php echo '<p>yeah!!</p>'; ?>
 <img src="images/IMG_0202.JPG"/>
 </body>
 </html>
